954a35e1acd330416517edae84fd32691ba89d0f
Randy Zwitch <rzwitch@gmail.com>
2024-04-26 11:42:07 -0400

    Update paths

--- data/pkg_downloads_by_day.R
+++ data/pkg_downloads_by_day.R
@@ -1,4 +1,4 @@
-library(httr2)
+library(httr)
 library(dplyr)
 library(cranlogs)
 source("github_extract.R")
@@ -20,7 +20,7 @@ get_pypi_daily_downloads <- function(pkg) {
 pkgs <- c("great_tables", "shiny", "vetiver", "plotnine", "siuba")
 pydf <- bind_rows(lapply(pkgs, get_pypi_daily_downloads))
 
-write.csv(pydf, "data/pypi_stats.csv", row.names = FALSE)
+#write.csv(pydf, "pypi_stats.csv", row.names = FALSE)
 
 #---
 
@@ -28,15 +28,15 @@ write.csv(pydf, "data/pypi_stats.csv", row.names = FALSE)
 rpkgs <- cran_downloads(c("shiny", "gt", "vetiver", "webR"),
                         from = "2017-01-01")
 
-write.csv(rpkgs, "data/rpkgs.csv", row.names = FALSE)
+#write.csv(rpkgs, "rpkgs.csv", row.names = FALSE)
 
 #tidyverse packages
 tidyvpkgs <- cran_downloads(tidyverse_packages(FALSE), from = "2017-01-01")
-write.csv(tidyvpkgs, "data/tidyverse_pkgs.csv", row.names = FALSE)
+#write.csv(tidyvpkgs, "tidyverse_pkgs.csv", row.names = FALSE)
 
 #tidymodels packages - need to validate with Max, don't seem to be accounting for 42 packages as he does
 tidympkgs <-  cran_downloads(tidymodels_packages(FALSE), from = "2017-01-01")
-write.csv(tidympkgs, "data/tidymodels_pkgs.csv", row.names = FALSE)
+#write.csv(tidympkgs, "tidymodels_pkgs.csv", row.names = FALSE)
 
 
 #r-lib packages
@@ -46,11 +46,11 @@ rlib_repos <- repo_list("r-lib") %>% table_format()
 notcran <- c("r-azure-pipelines", "r-lib.github.io", "repository-dispatch", "tree-sitter-r", "homebrew-taps", "homebrew-rig", "r-svn")
 cran_downloads(setdiff(rlib_repos$name, notcran), from = "2017-01-01") %>% 
   bind_rows() -> rlib_data
-write.csv(rlib_data, "data/rlib_pkgs.csv", row.names = FALSE)
+#write.csv(rlib_data, "rlib_pkgs.csv", row.names = FALSE)
 
 #r connectivity
 rcondf <- cran_downloads(c("sparklyr", "tensorflow", "keras", "odbc", "reticulate"), from = "2017-01-01")
-write.csv(rcondf, "data/rcon_pkgs.csv", row.names = FALSE)
+#write.csv(rcondf, "rcon_pkgs.csv", row.names = FALSE)
 
 
 #----RStudio downloads
