97c8bcb866ede34a472abc690df908e82ca3009a
Charlotte Wickham <cwickham@gmail.com>
2024-05-29 10:48:26 -0700

    Add md format

--- ignore-layout.lua
+++ ignore-layout.lua
@@ -1,8 +1,10 @@
-function Div(el)
-  if el.classes:includes('page-blue') or 
-    el.classes:includes('cols-2') or 
-    el.classes:includes('banner') or
-    el.classes:includes('back-page') then
-    return el.content
+if (quarto.doc.is_format('html') or quarto.doc.is_format('markdown')) then
+  function Div(el)
+    if el.classes:includes('page-blue') or 
+      el.classes:includes('cols-2') or 
+      el.classes:includes('banner') or
+      el.classes:includes('back-page') then
+      return el.content
+    end
   end
 end
