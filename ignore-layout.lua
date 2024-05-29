8f3b56097e2d5acd3f9c5ee358c964e267bb433a
Charlotte Wickham <cwickham@gmail.com>
2024-05-29 13:21:38 -0700

    Ignore divs for NOT typst

--- ignore-layout.lua
+++ ignore-layout.lua
@@ -1,4 +1,4 @@
-if (quarto.doc.is_format('html') or quarto.doc.is_format('markdown')) then
+if not quarto.doc.is_format('typst') then
   function Div(el)
     if el.classes:includes('page-blue') or 
       el.classes:includes('cols-2') or 
